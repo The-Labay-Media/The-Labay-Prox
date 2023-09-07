ad40189c38c37944da4ff40dc9d8530753a92081
MyBooty165 <126267638+MyBooty165@users.noreply.github.com>
2023-09-07 16:22:21 -0500

    Create index.mjs

---
+++ index.mjs
@@ -0,0 +1,45 @@
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { createBareServer } from '@tomphttp/bare-server-node';
+import serveStatic from 'serve-static';
+
+const httpServer = createServer();
+
+// Run the Bare server in the /bare/ namespace. This will prevent conflicts between the static files and the bare server.
+const bareServer = createBareServer('/bare/');
+
+const serve = serveStatic(
+	fileURLToPath(new URL('../public/', import.meta.url)),
+	{
+		fallthrough: false,
+	}
+);
+
+httpServer.on('request', (req, res) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeRequest(req, res);
+	} else {
+		serve(req, res, (err) => {
+			res.writeHead(err?.statusCode || 500, {
+				'Content-Type': 'text/plain',
+			});
+			res.end(err?.stack);
+		});
+	}
+});
+
+httpServer.on('upgrade', (req, socket, head) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
+});
+
+httpServer.on('listening', () => {
+	console.log('The Labay Prox');
+});
+
+httpServer.listen({
+	port: 8080,
+});
